b7b59dcab4c9029ca39f771741df9a6467314336
David Vázquez <davazp@es.gnu.org>
2010-02-08 03:34:00 +0100

    Remove package definition.

--- cl-icalendar.lisp
+++ cl-icalendar.lisp
@@ -18,12 +18,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with cl-icalendar.  If not, see <http://www.gnu.org/licenses/>.
 
-(defpackage :cl-icalendar
-  (:nicknames :icalendar)
-  (:use :cl :trivial-gray-streams)
-  (:export #:make-folding-stream
-           #:with-folding-stream))
-
 (in-package :cl-icalendar)
 
 ;;; Utilities
