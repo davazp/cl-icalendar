9a6b0ef8a69b831f15f1fcd5a2cd20471fb0d9d0
David Vázquez <davazp@es.gnu.org>
2010-05-24 20:34:39 +0200

    Property.lisp is rewritten. Provide property and property definition.

--- properties.lisp
+++ properties.lisp
@@ -20,101 +20,95 @@
 
 (in-package :cl-icalendar)
 
-(defvar *properties* (make-hash-table :test #'equal))
+;;;; Property definition
 
-(defclass property-type ()
-  ((types :accessor %types
-	  :documentation "A list of the allowed types (As strings) in
-	  this property, the first is the default"
-	  :initarg :types)
-   (multival-p :accessor %multival-p
-	       :initarg :multival-p)))
+(defclass property-definition ()
+  ((name
+    :initform (required-arg)
+    :type symbol
+    :initarg :name
+    :reader property-definition-name)
+   (x-property-p
+    :type boolean
+    :initform nil
+    :reader property-definition-x-property-p)
+   (type
+    :initform t
+    :initarg :type
+    :reader property-definition-type)
+   (default-type
+    :initform nil
+    :initarg :default-type
+    :reader property-definition-default-type)
+   (required
+    :initform nil
+    :initarg :required
+    :reader property-definition-required-p)
+   ;; Default value if property is not specified.
+   (default
+    :initarg :default
+    :reader property-definition-default)
+   ;; Number of times which the property must be specified.
+   (count
+    :initform 1
+    :type (or null (integer 1 *))
+    :initarg :count
+    :reader property-definition-count)
+   (multiple-instance-p
+    :initform nil
+    :type boolean
+    :initarg :multiple-instance-p
+    :reader property-definition-multiple-instance-p)
+   (parameter-list
+    :initform nil
+    :type list
+    :initarg :parameter-list
+    :reader property-definition-parameter-list)
+   (allow-x-parameters-p
+    :initform nil
+    :type boolean
+    :initarg :allow-x-parameter-p
+    :reader property-definition-allow-x-parameters-p)))
+
+(defmethod print-object ((object property-definition) stream)
+  (print-unreadable-object (object stream :type t)
+    (princ (property-definition-name object) stream)))
+
+(defun make-property-from-definition (definition)
+  (declare (list definition))
+  (let ((property-name (car definition))
+        (property-options (cdr definition)))
+    (apply #'make-instance 'property-definition
+           :name property-name
+           property-options)))
 
 
+;;;; Properties
 
-(defgeneric default-type (x))
+(defclass property ()
+  ((class
+    :initform (required-arg)
+    :initarg :class
+    :reader property-class)
+   (parameters
+    :initform nil
+    :initarg :parameters
+    :accessor property-parameters)
+   (value
+    :initarg :value
+    :accessor property-value)))
 
-(defmethod default-type ((self property-type))
-  (first (%types self)))
+;;; Signal a type-error if the type of value is wrong.
+(defmethod (setf property-value) :around (value (prop property))
+  (let ((prop-type (property-definition-type (property-class prop))))
+    (if (typep value prop-type)
+        (call-next-method)
+        (error 'type-error
+               :expected-type prop-type
+               :datum value))))
 
-(defmethod default-type ((property-name string))
-  (default-type (gethash property-name *properties*)))
-
-(defmacro define-property (name
-			   types
-			   multival-p)
-  "Define a property type"
-  `(setf (gethash ,name *properties*)
-	 (make-instance 'property-type
-			:types ',types
-			:multival-p ,multival-p)))
-
-(defun parse-property-name (string)
-  (or (gethash string *properties*)
-      (error "Undefined property")))
-
-(defun process-property (content-line)
-  (declare (content-line content-line))
-  "Process a content-line property and return it value (or values in a
-list, if a multivalue property)"
-  (with-slots (name params value) content-line
-    (let* ((property (parse-property-name name))
-	   (type (default-type property))
-	   (value (if (%multival-p property)
-		      (parse-values value type)
-		      (parse-value value type))))
-      (values value name))))
-
-
-;;; Property database
-
-;; Calendar properties
-(define-property "CALSCALE" ("TEXT") nil)
-(define-property "METHOD" ("TEXT") nil)
-(define-property "PRODID" ("TEXT") nil)
-(define-property "VERSION" ("TEXT") nil)
-
-;; Component properties
-(define-property "ATTACH" ("URI" "BINARY") nil)
-(define-property "CATEGORIES" ("TEXT") t)
-(define-property "CLASS" ("TEXT")  nil)
-(define-property "COMMENT" ("TEXT") nil)
-(define-property "DESCRIPTION" ("TEXT") nil)
-(define-property "GEO" ("FLOAT") t) ; Must be 2 comma separated values
-(define-property "LOCATION" ("TEXT") nil)
-(define-property "PERCENT-COMPLETE" ("INTEGER") nil)
-(define-property "PRIORITY" ("INTEGER") nil) ; 0 to 9 (Inclusive),
-(define-property "RESOURCES" ("TEXT") nil)
-(define-property "STATUS" ("TEXT") nil)
-(define-property "SUMMARY" ("TEXT") nil)
-(define-property "COMPLETED" ("DATE-TIME") nil)
-(define-property "DTEND" ("DATE-TIME" "DATE") nil)
-(define-property "DUE" ("DATE-TIME" "DATE") nil)
-(define-property "DTSTART" ("DATE-TIME" "DATE") nil)
-(define-property "DURATION" ("DURATION") nil)
-(define-property "FREEBUSY" ("PERIOD") t)
-(define-property "TRANSP" ("TEXT") nil)
-(define-property "TZID" ("TEXT") nil)
-(define-property "TZNAME" ("TEXT") nil)
-(define-property "TZOFFSETFROM" ("UTC-OFFSET") nil)
-(define-property "TZOFFSETTO" ("UTC-OFFSET") nil)
-(define-property "TZURL" ("URI") nil)
-(define-property "ATTENDEE" ("CAL-ADDRESS") nil)
-(define-property "CONTACT" ("TEXT") nil)
-(define-property "ORGANIZER" ("CAL-ADRESS") nil)
-(define-property "RECURRENCE-ID" ("DATE-TIME" "DATE") nil)
-(define-property "RELATED-TO" ("TEXT") nil)
-(define-property "URL" ("URI") nil)
-(define-property "UID" ("TEXT") nil)
-(define-property "EXDATE" ("DATE-TIME" "DATE") t)
-(define-property "RDATE" ("DATE-TIME" "DATE" "PERIOD") t)
-(define-property "RRULE" ("RECUR") nil)
-(define-property "ACTION" ("TEXT") nil)
-(define-property "REPEAT" ("INTEGER") nil)
-(define-property "TRIGGER" ("DURATION" "DATE-TIME") nil) ; UTC
-(define-property "CREATED" ("DATE-TIME") nil)
-(define-property "DTSTAMP" ("DATE-TIME") nil)
-(define-property "LAST-MODIFIED" ("DATE-TIME") nil)
-(define-property "SEQUENCE" ("INTEGER") nil)
+(defun make-property (class)
+  (declare (property-definition class))
+  (make-instance 'property :class class))
 
 ;; properties.lisp ends here
