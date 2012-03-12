a74400765466da07806994e7a27fc8a58c5be317
David Vázquez <davazp@gmail.com>
2012-03-12 20:56:34 +0000

    New error conditions

--- error.lisp
+++ error.lisp
@@ -1,6 +1,6 @@
 ;; error.lisp --- Error handling machinery
 ;;
-;; Copyrigth (C) 2010 David Vázquez
+;; Copyrigth (C) 2010, 2012 David Vázquez
 ;;
 ;; This file is part of cl-icalendar.
 ;;
@@ -19,15 +19,54 @@
 
 (in-package :cl-icalendar)
 
-;;; It is signaled when if an error ocurred while parsing iCalendar
-;;; values, properties or components.
-(define-condition icalendar-parse-error (simple-error)
+(define-condition icalendar-condition ()
   nil)
+
+(define-condition icalendar-error (icalendar-condition error)
+  nil)
+
+(define-condition icalendar-type-error (icalendar-error)
+  ((expected)
+   (found)
+   (place)))
+
+(define-condition icalendar-parse-error (icalendar-error)
+  ((stream :initarg :stream :reader icalendar-parse-error-stream)
+   (line :initarg :line :reader icalendar-parse-error-line)
+   (column :initarg :column :reader icalendar-parse-error-column)))
+
+(define-condition icalendar-warning (icalendar-condition warning)
+  nil)
+
+(define-condition validation-error (icalendar-error)
+  ((component :initarg :component :reader validation-error-component)))
+
+(define-condition missing-property (validation-error)
+  ((property-name)))
+
+(define-condition invalid-parameter (validation-error)
+  ((property)
+   (parameter)))
+
+(define-condition invalid-property (validation-error)
+  ((component)
+   (property)))
+
+(define-condition invalid-component (validation-error)
+  ((component)))
+
 
 ;;; Signal an icalendar-parse-error condition.
 (defun %parse-error (format &rest args)
   (error 'icalendar-parse-error
          :format-control format
          :format-arguments args))
+
+
+(defun ical-error (format &rest args)
+  (error 'icalendar-error
+         :format-control format
+         :format-arguments args))
+
 
 ;;; error.lisp ends here
